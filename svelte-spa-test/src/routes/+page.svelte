2a767d1c3c2802b3b059a09fa61fd93c533ab08c
Niall Mackinnon <mackinaut@gmail.com>
2026-03-17 15:43:28 -0700

    Add svelte app

---
+++ svelte-spa-test/src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+
+    let counter = $state(0);
+    let targetPage = $state("");
+</script>
+
+<div class="root-layout">
+<h1>Svelte SPA Test</h1>
+<p>Click the button to increment: {counter}</p>
+
+<div class="flex-column ">
+    <button onclick={() => counter++}>Increment</button>
+    <button onclick={() => counter = 0}>Reset</button>
+    <div>
+        <button onclick={() => targetPage && goto(`#/page${targetPage}`)}>Goto Page {targetPage}</button>
+        <select bind:value={targetPage}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+    </div>
+</div>
+</div>
+
+<style>
+    .root-layout {
+        margin: 0 auto;
+        max-width: 500px;
+    }
+
+    .flex-column {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
---
+++ svelte-spa-test/src/routes/page[num]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+
+    let { params } = $props();
+</script>
+
+<div class="root-layout">
+    <h1>Svelte SPA Test - Page {params.num}</h1>
+    <button onclick={() => goto("/")}>Go back</button>
+</div>
+
+<style>
+    .root-layout {
+        margin: 0 auto;
+        max-width: 500px;
+    }
+</style>
