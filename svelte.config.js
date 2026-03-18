73ca682d4a1c730465c5f310e8c95bcecc2c1dd9
Niall Mackinnon <mackinaut@gmail.com>
2026-03-18 14:06:21 -0700

    Use bundleStrategy 'inline' option

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		output: {
+			bundleStrategy: "inline"
+		},
 		router: {
 			type: "hash"
 		},
