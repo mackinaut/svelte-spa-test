db7a916842c8425bb1e0813033f2bba28722943f
Niall Mackinnon <mackinaut@gmail.com>
2026-03-18 13:45:05 -0700

    Move app up one directory

--- svelte.config.js
+++ svelte.config.js
@@ -11,10 +11,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: 'index.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/svelte-spa-test'
-		}
+		})
 	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
